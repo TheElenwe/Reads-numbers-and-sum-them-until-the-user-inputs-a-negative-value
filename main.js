2eb6981161414a0c8402407d812d7fbe78abfe88
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-14 22:31:35 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,9 @@
-var readline = require("readline");
+const prompt = require("prompt-sync")({ sigint: true });
 
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-var recursiveAsyncReadLine = function () {
-  rl.question("Give number : ", function (answer) {
-    total=0;
-    for (i = 0; i <answer; i++) 
-    total = total + (i + 1);
-  
+while (true){
+    number = prompt("Enter a number")
+    if (number == '-5')
     
-    if (answer < 0)
-      //we need some base case, for recursion
-
-      return rl.close(); //closing RL and returning from function.
-
-    console.log(`The sum is : ${answer} ${total}`);
-    recursiveAsyncReadLine(); //Calling this function again to ask new question
-  });
-};
-
-recursiveAsyncReadLine(); //we have to actually start our recursion somehow
+        break;
+    
+}
